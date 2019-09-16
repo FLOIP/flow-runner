639779c4ebf341a6a47cb709eb90576119bb98da
bzabos <b.zabos@gmail.com>
2019-09-16 15:32:59 -0600

    VMO-1119 Build + pkg v0.0.27

---
+++ dist/domain/Expression.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Expression {
+    constructor(evalContext) {
+        this.evalContext = evalContext;
+    }
+    evaluate() {
+    }
+}
+exports.default = Expression;
+//# sourceMappingURL=Expression.js.map
