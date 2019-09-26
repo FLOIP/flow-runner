d64ab5007858d71f66decd783f4e7055f2500559
bzabos <b.zabos@gmail.com>
2019-09-26 13:56:05 -0600

    VMO-1164 Build + pkg v0.0.30

--- dist/flow-spec/Context.d.ts
+++ dist/flow-spec/Context.d.ts
@@ -1,27 +1,27 @@
-import IContext, { CursorType } from './IContext';
+import IContext from './IContext';
 import IFlow from './IFlow';
 import IContact from './IContact';
 import DeliveryStatus from './DeliveryStatus';
 import IBlockInteraction from './IBlockInteraction';
 import { IResource, IResources, SupportedMode } from '..';
 export default class Context implements IContext {
-    contact: IContact;
-    createdAt: string;
-    cursor: CursorType;
-    deliveryStatus: DeliveryStatus;
-    entryAt: string;
-    exitAt: string;
-    firstFlowId: string;
-    flows: IFlow[];
     id: string;
-    interactions: IBlockInteraction[];
-    languageId: string;
+    createdAt: string;
+    deliveryStatus: DeliveryStatus;
     mode: SupportedMode;
-    nestedFlowBlockInteractionIdStack: string[];
-    resources: IResources;
+    languageId: string;
+    contact: IContact;
     sessionVars: object;
-    userId: string;
-    constructor(contact: IContact, createdAt: string, cursor: CursorType, deliveryStatus: DeliveryStatus, entryAt: string, exitAt: string, firstFlowId: string, flows: IFlow[], id: string, interactions: IBlockInteraction[], languageId: string, mode: SupportedMode, nestedFlowBlockInteractionIdStack: string[], resources: IResources, sessionVars: object, userId: string);
+    interactions: IBlockInteraction[];
+    nestedFlowBlockInteractionIdStack: string[];
+    flows: IFlow[];
+    firstFlowId: string;
+    resources: IResources;
+    entryAt?: string | undefined;
+    exitAt?: string | undefined;
+    userId?: string | undefined;
+    cursor?: [string, (import("..").IPromptConfig<any> & import("..").IBasePromptConfig) | undefined] | undefined;
+    constructor(id: string, createdAt: string, deliveryStatus: DeliveryStatus, mode: SupportedMode, languageId: string, contact: IContact, sessionVars: object, interactions: IBlockInteraction[], nestedFlowBlockInteractionIdStack: string[], flows: IFlow[], firstFlowId: string, resources: IResources, entryAt?: string | undefined, exitAt?: string | undefined, userId?: string | undefined, cursor?: [string, (import("..").IPromptConfig<any> & import("..").IBasePromptConfig) | undefined] | undefined);
     getResource(resourceId: string): IResource;
 }
 //# sourceMappingURL=Context.d.ts.map
