12ed3dc7dd8f9cf448ecb78d879265997d18f959
bzabos <b.zabos@gmail.com>
2019-10-10 10:20:15 -0600

    VMO-1150 Reuse evalContext generator between exit test expression and text resource expressions.

--- __tests__/runners/SelectOneResponseBlockRunner.spec.ts
+++ __tests__/runners/SelectOneResponseBlockRunner.spec.ts
@@ -1,23 +1,24 @@
 import SelectOneResponseBlockRunner from '../../src/domain/runners/SelectOneResponseBlockRunner'
-import IContext, {CursorInputRequiredType} from '../../src/flow-spec/IContext'
-// import IDataset, {createDefaultDataset} from '../../__test_fixtures__/fixtures/IDataset'
+import IContext, {findBlockOnActiveFlowWith, findInteractionWith} from '../../src/flow-spec/IContext'
 import IBlockExit from '../../src/flow-spec/IBlockExit'
 import ISelectOneResponseBlock from '../../src/model/block/ISelectOneResponseBlock'
+import IDataset, {createDefaultDataset} from '../../__test_fixtures__/fixtures/IDataset'
 import IContact from '../../src/flow-spec/IContact'
-import IBlockInteraction from '../../src/flow-spec/IBlockInteraction'
-import {ISelectOnePromptConfig} from '../../src'
-import IFlow from '../../src/flow-spec/IFlow'
 
 describe('SelectOneResponseBlockRunner', () => {
-  // let dataset: IDataset
+  let dataset: IDataset
 
   beforeEach(() => {
-    // dataset = createDefaultDataset()
+    dataset = createDefaultDataset()
   })
 
   describe('run', () => {
     it('sanity // should return an exit when some exist', () => {
-      const block: ISelectOneResponseBlock = {
+      const ctx: IContext = dataset.contexts[1];
+      (ctx.contact as IContactWithAge).age = 12
+
+      const interaction = findInteractionWith(ctx.cursor![0], ctx)
+      const block: ISelectOneResponseBlock = Object.assign(findBlockOnActiveFlowWith(interaction.blockId, ctx), {
         exits: [
           {test: '@(contact.age > 73)'} as IBlockExit,
           {test: '@(contact.age > 50)'} as IBlockExit,
@@ -25,9 +26,8 @@ describe('SelectOneResponseBlockRunner', () => {
           {test: '@(contact.age > 8)'} as IBlockExit,
           {test: '@(contact.age > 0)'} as IBlockExit,
         ]
-      } as ISelectOneResponseBlock
+      }) as ISelectOneResponseBlock
 
-      const ctx: IContext = createContext()
       const runner = new SelectOneResponseBlockRunner(block, ctx)
       const exit: IBlockExit = runner.run()
       expect(exit).toBe(block.exits[3])
@@ -37,21 +37,6 @@ describe('SelectOneResponseBlockRunner', () => {
   })
 })
 
-function createContext(): IContext {
-  return  {
-    contact: {
-      id: 'contact-123',
-      name: 'Bert',
-      age: 12,
-    } as IContact,
-
-    nestedFlowBlockInteractionIdStack: [] as string[],
-    firstFlowId: 'flow-1234',
-    flows: [{uuid: 'flow-1234'}] as IFlow[],
-
-    cursor: [
-      {} as IBlockInteraction,
-      {} as ISelectOnePromptConfig,
-    ] as any as CursorInputRequiredType,
-  } as IContext
+interface IContactWithAge extends IContact {
+  age: number
 }
