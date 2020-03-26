17c3a620936a2d4f6a6702e04c3ee9a78c28263d
Roan Cooney <roan.cooney@viamo.io>
2020-03-26 15:17:48 -0600

    Build + package for release.

--- dist/flow-spec/IContact.d.ts
+++ dist/flow-spec/IContact.d.ts
@@ -1,7 +1,6 @@
 export interface IContact {
     id: string;
-    name: string;
-    createdAt: string;
+    [key: string]: string | undefined;
 }
 export default IContact;
 //# sourceMappingURL=IContact.d.ts.map
