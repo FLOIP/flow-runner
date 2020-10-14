00e9d959c96c3bf48444604ce45e2beca8c40a0d
Kyle Seifert <kyle.seifert@viamo.io>
2020-10-14 10:32:59 -0600

    Build + package for release.

--- dist/model/block/IAdvancedSelectOneBlock.js
+++ dist/model/block/IAdvancedSelectOneBlock.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ADVANCED_SELECT_ONE_BLOCK_TYPE = void 0;
-exports.ADVANCED_SELECT_ONE_BLOCK_TYPE = 'Vendor\\io_viamo\\DirectorySelectionBlock';
+exports.ADVANCED_SELECT_ONE_BLOCK_TYPE = 'MobilePrimitives\\Extended\\AdvancedSelectOne';
 //# sourceMappingURL=IAdvancedSelectOneBlock.js.map
