2c97b60b060553e3d4034d387f7b81f78d5e1332
Roan Cooney <roan.cooney@viamo.io>
2020-10-27 15:05:31 -0600

    master - bugfix, wrong SupportedContentType character casing.

--- src/domain/SupportedContentType.ts
+++ src/domain/SupportedContentType.ts
@@ -3,9 +3,9 @@
  * `SupportedContentType.addCustomSupportedContentType()`.
  */
 export class SupportedContentType {
-  static TEXT = 'TEXT'
-  static AUDIO = 'AUDIO'
-  static IMAGE = 'IMAGE'
-  static VIDEO = 'VIDEO'
-  static CSV = 'CSV'
+  static TEXT = 'text'
+  static AUDIO = 'audio'
+  static IMAGE = 'image'
+  static VIDEO = 'video'
+  static CSV = 'csv'
 }
