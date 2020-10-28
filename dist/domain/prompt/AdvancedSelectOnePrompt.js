7d094ac11585eb4297f4c251f08fd12e8ab67a7b
Roan Cooney <roan.cooney@viamo.io>
2020-10-28 15:59:00 -0600

    Build + package for release.

--- dist/domain/prompt/AdvancedSelectOnePrompt.js
+++ dist/domain/prompt/AdvancedSelectOnePrompt.js
@@ -4,23 +4,7 @@ exports.AdvancedSelectOnePrompt = exports.ADVANCED_SELECT_ONE_PROMPT_KEY = void 
 const __1 = require("../..");
 exports.ADVANCED_SELECT_ONE_PROMPT_KEY = 'AdvancedSelectOne';
 class AdvancedSelectOnePrompt extends __1.BasePrompt {
-    validate(selectedRow, choiceRows) {
-        const { choiceRowFields, isResponseRequired } = this.config;
-        __1.assertNotNull(choiceRows, () => 'choiceRows must be non-null', message => new __1.ValidationException(message));
-        if (isResponseRequired) {
-            const hasSelectedRow = choiceRows.some(row => selectedRow === null || selectedRow === void 0 ? void 0 : selectedRow.every(selection => {
-                const columnIndex = choiceRowFields.indexOf(selection.name);
-                if (columnIndex < 0) {
-                    throw new __1.ValidationException(`Failed to find a column called: ${selection.name}`);
-                }
-                else {
-                    return selection.value === row[columnIndex];
-                }
-            }));
-            if (!hasSelectedRow) {
-                throw new __1.ValidationException(`Failed to find the given row: ${selectedRow}`);
-            }
-        }
+    validate() {
         return true;
     }
 }
