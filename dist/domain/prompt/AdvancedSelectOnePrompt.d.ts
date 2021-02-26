80bca51673956ab26e964b96a9195ecc071e1712
Roan Cooney <roan.cooney@viamo.io>
2021-02-26 15:51:10 -0600

    Build + package for release.

--- dist/domain/prompt/AdvancedSelectOnePrompt.d.ts
+++ dist/domain/prompt/AdvancedSelectOnePrompt.d.ts
@@ -1,6 +1,6 @@
-import { BasePrompt, IAdvancedSelectOne, IAdvancedSelectOnePromptConfig } from '../..';
+import { BasePrompt, IAdvancedSelectOnePromptConfig } from '../..';
 export declare const ADVANCED_SELECT_ONE_PROMPT_KEY = "AdvancedSelectOne";
 export declare class AdvancedSelectOnePrompt extends BasePrompt<IAdvancedSelectOnePromptConfig> {
-    validate(selectedRow?: IAdvancedSelectOne[], choiceRows?: string[][]): boolean;
+    validate(): boolean;
 }
 //# sourceMappingURL=AdvancedSelectOnePrompt.d.ts.map
