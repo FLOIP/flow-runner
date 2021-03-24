7ba569086e6257b9885238516c0be7a36b664d79
Kyle Seifert <kyle.seifert@viamo.io>
2021-03-24 13:58:10 -0600

    build

--- dist/flow-spec/Group.d.ts
+++ dist/flow-spec/Group.d.ts
@@ -1,15 +1,14 @@
 import { IContactGroup, IGroup } from '..';
 export declare class Group implements IGroup {
     group_key: string;
-    label?: string | undefined;
-    constructor(group_key: string, label?: string | undefined);
+    label?: string;
+    constructor(group_key: string, label?: string);
     get __value__(): string;
 }
 export declare class ContactGroup extends Group implements IContactGroup {
     updated_at: string;
-    deleted_at?: string | undefined;
-    constructor(group: IGroup);
-    constructor(group_key: string, label: string, updated_at: string, deleted_at?: string);
-    constructor(group_key: string, updated_at: string, deleted_at?: string);
+    deleted_at?: string;
+    constructor(group: IGroup, updated_at?: string, deleted_at?: string, label?: string);
+    constructor(group_key: string, updated_at?: string, deleted_at?: string, label?: string);
 }
 //# sourceMappingURL=Group.d.ts.map
