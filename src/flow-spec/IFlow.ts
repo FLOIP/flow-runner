1e86daacf09ff1b41cde119590f812c0aa154fd8
safydy <r.safydy@gmail.com>
2021-04-20 13:10:32 -0600

    update jsonschema annotations for IFlow.ts

--- src/flow-spec/IFlow.ts
+++ src/flow-spec/IFlow.ts
@@ -27,7 +27,7 @@ export interface IFlow {
   /**
    * A globally unique identifier for this Flow.  (See UUID Format: https://floip.gitbook.io/flow-specification/flows#uuid-format)
    *
-   * @TJS-pattern ^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$
+   * @pattern ^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$
    */
   // UUID32
   uuid: string
@@ -45,7 +45,7 @@ export interface IFlow {
   /**
    * The time when this flow was last modified, in UTC, with microsecond precision: "2016-12-25 13:42:05.234598"
    *
-   * @TJS-format date-time
+   * @format date-time
    */
   // UTC like: 2016-12-25 13:42:05.234598
   last_modified: string
@@ -53,7 +53,8 @@ export interface IFlow {
   /**
    * The number of seconds of inactivity after which Contact input for this flow is no longer accepted, and Runs in progress are terminated
    *
-   * @TJS-type integer
+   * @minimum 0
+   * @type integer // TODO: this is not working, nor: @type `integer`
    */
   interaction_timeout: number
 
@@ -65,24 +66,30 @@ export interface IFlow {
 
   /**
    * A list of the supported Modes that the Flow has content suitable for.
+   *
+   * @minItems 1
    */
   supported_modes: SupportedMode[]
 
   /**
    * A list of the languages that the Flow has suitable content for.
    * See language object specification: https://floip.gitbook.io/flow-specification/flows#language-objects-and-identifiers.
+   *
+   * @minItems 1
    */
   languages: ILanguage[]
 
   /**
    * A list of the Blocks in the flow.
+   *
+   * @minItems 1
    */
   blocks: IBlock[]
 
   /**
    * The ID of the block in blocks that is at the beginning of the flow.
    *
-   * @TJS-pattern ^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$
+   * @pattern ^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$
    */
   first_block_id: string
 
@@ -90,7 +97,7 @@ export interface IFlow {
    * If provided, the ID of the block in blocks that will be jumped to if there is an error or deliberate exit condition during Flow Run.
    * If not provided, the Flow Run will end immediately.
    *
-   * @TJS-pattern ^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$
+   * @pattern ^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}$
    */
   exit_block_id?: string
 }
