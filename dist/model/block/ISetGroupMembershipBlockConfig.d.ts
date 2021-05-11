562e24208b7904d7cb21de2d541f54b366411ada
Bulat Gabidullin <bulatgabidullin@gmail.com>
2021-05-11 13:59:13 -0600

    hotfix/VMO-3998/EditGroupMembershipFix - Fixed

--- dist/model/block/ISetGroupMembershipBlockConfig.d.ts
+++ dist/model/block/ISetGroupMembershipBlockConfig.d.ts
@@ -1,6 +1,6 @@
 export interface ISetGroupMembershipBlockConfig {
     group_key: string;
     group_name?: string;
-    is_member: string;
+    is_member: boolean;
 }
 //# sourceMappingURL=ISetGroupMembershipBlockConfig.d.ts.map
--- src/model/block/ISetGroupMembershipBlockConfig.ts
+++ src/model/block/ISetGroupMembershipBlockConfig.ts
@@ -1,5 +1,5 @@
 export interface ISetGroupMembershipBlockConfig {
   group_key: string
   group_name?: string
-  is_member: string
+  is_member: boolean
 }
