96deaf6efa904766eb44a0da1506a8510be3eacd
Mark Boots <mark.boots@viamo.io>
2021-07-23 16:25:04 -0600

    Change type of ISetGroupMembershipBlockConfig.is_member to string (for expressions)

--- src/model/block/ISetGroupMembershipBlockConfig.ts
+++ src/model/block/ISetGroupMembershipBlockConfig.ts
@@ -1,7 +1,17 @@
 import {IBlockConfig} from './IBlockConfig'
 
 export interface ISetGroupMembershipBlockConfig extends IBlockConfig {
+  /**
+   * An identifier for the group that membership will be set within.
+   */
   group_key: string
+  /**
+   * A human-readable label in addition to the group_key, in cases where the group_name needs to be displayed to the Contact.
+   */
   group_name?: string
-  is_member: boolean
+  /**
+   * Determines the membership state: falsy to remove the contact from the group,
+   * truthy to add, and null for no change to the existing membership.
+   */
+  is_member: string
 }
