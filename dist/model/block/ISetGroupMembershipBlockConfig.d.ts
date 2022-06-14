39a313f4ab383a1f91d3bf9f4a39f1732c2cff4f
Nikolai Kim <nikolai.kim@viamo.io>
2022-06-14 13:59:56 +0700

    VMO-6201 Update ISetGroupMembershipConfig

--- dist/model/block/ISetGroupMembershipBlockConfig.d.ts
+++ dist/model/block/ISetGroupMembershipBlockConfig.d.ts
@@ -1,7 +1,14 @@
 import { IBlockConfig } from '../..';
-export interface ISetGroupMembershipBlockConfig extends IBlockConfig {
+export interface IGroupMembership {
     group_key: string;
-    group_name?: string;
-    is_member: string;
+    group_name: string;
 }
+export interface IAddOrRemoveGroupMembership extends IBlockConfig {
+    is_member: boolean;
+    groups: IGroupMembership[];
+}
+export interface IClearGroupMembership extends IBlockConfig {
+    clear: true;
+}
+export declare type ISetGroupMembershipBlockConfig = IAddOrRemoveGroupMembership | IClearGroupMembership;
 //# sourceMappingURL=ISetGroupMembershipBlockConfig.d.ts.map
--- src/model/block/ISetGroupMembershipBlockConfig.ts
+++ src/model/block/ISetGroupMembershipBlockConfig.ts
@@ -1,23 +1,23 @@
 import {IBlockConfig} from '../..'
 
-export interface ISetGroupMembershipBlockConfig extends IBlockConfig {
-  /**
-   * An identifier for the group that membership will be set within.
-   */
+export interface IGroupMembership {
   group_key: string
-  /**
-   * A human-readable label in addition to the group_key, in cases where the group_name needs to be displayed to the Contact.
-   */
-  group_name?: string
-  /**
-   * Determines the membership state:
-   * - falsy to remove the contact from the group,
-   * - truthy to add,
-   * - and null for no change to the existing membership.
-   *
-   * Its type is string because it may contain expressions too.
-   *
-   * @format floip-expression
-   */
-  is_member: string
+  group_name: string
 }
+
+/**
+ * Adding or removing contact from group(s)
+ */
+export interface IAddOrRemoveGroupMembership extends IBlockConfig {
+  is_member: boolean
+  groups: IGroupMembership[]
+}
+
+/**
+ * Removing contact from all groups
+ */
+export interface IClearGroupMembership extends IBlockConfig {
+  clear: true
+}
+
+export type ISetGroupMembershipBlockConfig = IAddOrRemoveGroupMembership | IClearGroupMembership
