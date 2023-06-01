79d5d74c96ec25df49c079af73724f980dda3dd2
Nikolai Kim <nikolai.kim@viamo.io>
2023-06-01 11:33:21 -0600

    CORE-571 Add user_added_groups to ui_metadata

--- src/model/block/ISetGroupMembershipBlock.ts
+++ src/model/block/ISetGroupMembershipBlock.ts
@@ -1,7 +1,20 @@
-import {IBlock, ISetGroupMembershipBlockConfig} from '../..'
+import {IBlock, IBlockVendorMetadata, IBlockVendorMetadataFloip, IFloipUIMetadata, ISetGroupMembershipBlockConfig} from '../..'
 
 export const SET_GROUP_MEMBERSHIP_BLOCK_TYPE = 'Core.SetGroupMembership'
 
+export interface ISetGroupMembershipFloipUiMetadata extends IFloipUIMetadata {
+  user_added_groups?: string[]
+}
+
+export interface ISetGroupMembershipVendorMetadataFloip extends IBlockVendorMetadataFloip {
+  ui_metadata: ISetGroupMembershipFloipUiMetadata
+}
+
+export interface ISetGroupMembershipVendorMetadata extends IBlockVendorMetadata {
+  floip: ISetGroupMembershipVendorMetadataFloip
+}
+
 export interface ISetGroupMembershipBlock extends IBlock<ISetGroupMembershipBlockConfig> {
   config: ISetGroupMembershipBlockConfig
+  vendor_metadata?: ISetGroupMembershipVendorMetadata
 }
